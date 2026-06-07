a0f823e0c49e903f2a0dfeacd9b634556461fa9a
Hongtruongbvn <106640293+Hongtruongbvn@users.noreply.github.com>
2026-06-07 21:45:56 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   // Kích hoạt CORS (Cross-Origin Resource Sharing)
   app.enableCors({
     // Cho phép cổng mặc định của Vite (http://localhost:5173) truy cập vào API
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173','https://profile-front-end-xi.vercel.app','https://profile-front-end-chi.vercel.app'], 
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173','https://profile-front-end-xi.vercel.app',
+             'https://profile-front-end-chi.vercel.app',
+            'https://profile.truongbvn.online'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
